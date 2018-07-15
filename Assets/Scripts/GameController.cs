9dd878609c3956af681a1a8f63896500f0d5c6b8
Alexander Scheurer <mail@aspepex.net>
2018-07-15 17:20:13 +0200

    Teleporting works

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance;
+
+	// Use this for initialization
+	void Start () {
+	    if (Instance == null)
+	    {
+	        Instance = this;
+	    }
+	}
+
+    public void Reload()
+    {
+        Debug.Log("Reloading Level");
+        SceneManager.LoadScene(0);
+    }
+
+}
