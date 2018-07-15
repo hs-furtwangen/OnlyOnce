4f6c13a4b231d215a7a8c94efcaded50ebf05e30
Alexander Scheurer <mail@aspepex.net>
2018-07-15 19:36:20 +0200

    Mostly working

---
+++ Assets/Scripts/EnemyB.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyB : MonoBehaviour
+{
+    public float KillAt;
+
+    void OnCollisionEnter2D(Collision2D collison)
+    {
+        Debug.Log(collison.contacts[0].point.y - this.transform.position.y);
+
+        if (collison.contacts[0].point.y - this.transform.position.y > KillAt)
+        {
+            Destroy(this.gameObject);
+            PlayerPlatformerController.Instance.Bump();
+        }
+        else
+        {
+            PlayerPlatformerController.Instance.Kill();
+        }
+    }
+}
